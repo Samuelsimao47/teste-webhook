f46972413bee0fae56efbfa1831bf1c87acf17fe
Samuel Simao <89145853+Samuelsimao47@users.noreply.github.com>
2021-11-22 08:36:44 -0300

    sem o body

--- main.js
+++ main.js
@@ -12,34 +12,34 @@ exports.aloha = (req, res) => {
 
     console.log('cheguei aqui');
 
-    // res.status(200).send(`O grêmio NÃO vai ser rebaixado`);
+    res.status(200).send(`O grêmio NÃO vai ser rebaixado`);
     
 
     // ------------------------------------------------------------------------
 
 
-    const strId = 'nada'
-    const url = "https://builder.knack.com/samuel/primeiro-teste/schema/list/objects/object_1/fields"
-    const authHeaders = {
-        headers: {
-            'X-Knack-Application-Id': "618bac31c8cfc8001e2d489d",
-            'X-Knack-REST-API-KEY': "82edf690-a9d7-498e-b6e7-7e263a1391d0",
-            'Content-Type': 'application/json'
-        },
-    }
+    // const strId = 'nada'
+    // const url = "https://builder.knack.com/samuel/primeiro-teste/schema/list/objects/object_1/fields"
+    // const authHeaders = {
+    //     headers: {
+    //         'X-Knack-Application-Id': "618bac31c8cfc8001e2d489d",
+    //         'X-Knack-REST-API-KEY': "82edf690-a9d7-498e-b6e7-7e263a1391d0",
+    //         'Content-Type': 'application/json'
+    //     },
+    // }
 
-    let body = {
-        field_1: 'Novo Hamburgo'
-    }
+    // let body = {
+    //     field_1: 'Novo Hamburgo'
+    // }
 
-    await axios.post(url, body, authHeaders)
-        .then(function (response) {
-            idRowCreated = response.data.id
-            if (logRetorno) { console.log("POST: " + logRetorno.green); }
-        })
-        .catch(function (error) {
-            console.log("error posting - ".red + logRetorno);
-            hasError = true
-        })
+    // await axios.post(url, body, authHeaders)
+    //     .then(function (response) {
+    //         idRowCreated = response.data.id
+    //         if (logRetorno) { console.log("POST: " + logRetorno.green); }
+    //     })
+    //     .catch(function (error) {
+    //         console.log("error posting - ".red + logRetorno);
+    //         hasError = true
+    //     })
 
 };
